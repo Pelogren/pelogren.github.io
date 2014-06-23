763d4efdefc141c1832bc7eb8f7ebad17d0b6ab5
Lorenz Prasch <lorenz.prasch@gmail.com>
2014-06-23 14:29:27 +0200

    site change works

--- assets/js/myjQuery.js
+++ assets/js/myjQuery.js
@@ -5,6 +5,8 @@ function toDaily() {
 	$("#daily").addClass('activeOverview');   
 	$("#monthly").removeClass('activeOverview');
 	$("#periodly").removeClass('activeOverview');
+	$("#budget-tabs").find('li').css("border-bottom", "none");
+	$("a.activeOverview").parents('li').css("border-bottom", "4px solid #344E5B");
  };
 
  function toMonthly() {
@@ -14,6 +16,8 @@ function toDaily() {
    	$("#daily").removeClass('activeOverview');   
 	$("#monthly").addClass('activeOverview');
 	$("#periodly").removeClass('activeOverview');
+	$("#budget-tabs").find('li').css("border-bottom", "none");
+	$("a.activeOverview").parents('li').css("border-bottom", "4px solid #344E5B");
  };
 
  function toPeriod() {
@@ -23,19 +27,32 @@ function toDaily() {
    	$("#daily").removeClass('activeOverview');   
 	$("#monthly").removeClass('activeOverview');
 	$("#periodly").addClass('activeOverview');
+	$("#budget-tabs").find('li').css("border-bottom", "none");
+	$("a.activeOverview").parents('li').css("border-bottom", "4px solid #344E5B");
  };
 
-
-$("#dailyOverview").on("swipeleft",toMonthly());
-$("#monthlyOverview").on("swipeleft",toPeriod());
-$("#periodOverview").on("swiperight",toMonthly());
-$("#monthlyOverview").on("swiperight",toDaily());
+$(function() {
+	$("#dailyOverview").on("swipeleft",toMonthly);
+	$("#monthlyOverview").on("swipeleft",toPeriod);
+	$("#periodOverview").on("swiperight",toMonthly);
+	$("#monthlyOverview").on("swiperight",toDaily);
+	$("a.activeOverview").parents('li').css("border-bottom", "4px solid #344E5B");
+});
 
 // change color of the navbar elements on click
 
 $(function() {  
 $('.navlink').click(function(){
 	$('.navlink').removeClass('activeSite');
+	$('.page').removeClass('activePage');
     $(this).addClass('activeSite');
+    $(this.hash).addClass('activePage');
   });
+});
+
+// functions executed on loading of the Site
+
+$( document ).on( "mobileinit", function() {
+	// hides the loading message
+	// $.mobile.loadingMessage = false;
 });
