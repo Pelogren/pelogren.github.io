87a7c17139bf968258e8ff6f97f5192fa71fc5ae
Lorenz Prasch <lorenz.prasch@gmail.com>
2014-07-09 14:57:43 +0200

    reinstalled budgetbutler

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,19 +1,19 @@
 var categories = [
 						{ //EDUCATION
-							name:'FRUIT',
+							name:'Education',
 							color:'#D95246', //red
 							limit: '100',
 							expenses: [ 
-										{name: 'Banana',
-										 price: 5,
+										{name: 'Bear Neurowissenschaften',
+										 price: 98.90,
 										 date: '10.6.14',
 										},
-										{name: 'Tomato',
-										 price: 3,
+										{name: 'Schöning Theo. Informatik',
+										 price: 19.90,
 										 date: '14.6.14',
 										},
-										{name: 'Lettuce',
-										 price: 1,
+										{name: 'Bleistift',
+										 price: 0.90,
 										 date: '16.6.14',
 										}
 										]
@@ -21,49 +21,49 @@ var categories = [
 
 
 						{//FOOD
-							name:'DIARY',
+							name:'Food',
 							color:'#2A2931', //dunkeldunkelblau
 							limit: '220',
 							expenses: [ 
-										{name: 'Parmesan Cheese',
-										 price: 1,
+										{name: 'Einkauf Aldi',
+										 price: 17.96,
 										 date: '11.6.14',
 										},
-										{name: 'Yoghurt',
-										 price: 3,
+										{name: 'Döner',
+										 price: 3.50,
 										 date: '14.6.14',
 										},
-										{name: 'Creme',
-										 price: 2,
+										{name: 'Einkauf Rewe',
+										 price: 20.50 ,
 										 date: '16.6.14',
 										}
 										]
 						},
 						{//Entertainment
-							name:'MEAT',
+							name:'Entertainment',
 							color:'#53798D', //hellblau
 							limit: '80',
 							expenses: [ 
-										{name: 'Tuna',
-										 price: 2,
+										{name: 'Biergarten',
+										 price: 17.60,
 										 date: '5.6.14',
 										},
-										{name: 'BBQ Steak',
-										 price: 1,
+										{name: 'Kino mit James',
+										 price: 13.50,
 										 date: '14.6.14',
 										}
 										]},
 						{//Transportation
-							name:'BEVERAGES',
+							name:'Entertainment',
 							color:'#53798D', //hellblau
 							limit: '80',
 							expenses: [ 
-										{name: 'Beer',
-										 price: 7,
+										{name: 'Semesterticket SS14',
+										 price: 141.00,
 										 date: '5.6.14',
 										},
-										{name: 'Milk',
-										 price: 2,
+										{name: 'Taxi Ulm',
+										 price: 13.50,
 										 date: '11.6.14',
 										}
 
@@ -76,20 +76,20 @@ var categories = [
 function createExpenseEntry(category){
 
 		var begintable= "<table>";
-		var tablerows= "<thead><tr><th>ingredients</th><th class=\"listPrice\">amount</th><th></th></tr></thead><tbody>";
+		var tablerows= "<thead><tr><th>Expense</th><th class=\"listPrice\">Price</th></tr></thead><tbody>";
 		var endtable= "</table>";
 		var sum= 0; 
-		var addbutton= "<button type=\"button\" class=\"btn btn-primary btn-lg btn-block flying\" data-toggle=\"modal\" data-target=\"#addExpenseModal\">NEW ENTRY</button>"; 
+		var addbutton= "<button type=\"button\" class=\"btn btn-primary btn-lg btn-block flying\" data-toggle=\"modal\" data-target=\"#addExpenseModal\">ADD NEW EXPENSE</button>"; 
 
 	for (var element in categories[category].expenses) {  //erstellt für jedes Element in Expenses einer bestimmten Kategorie (categorie) einen Eintrag
 		var sum= sum + categories[category].expenses[element].price;
-		var tablerow = "<tr><td>" + categories[category].expenses[element].name + "</td><td class=\"listPrice\">" + categories[category].expenses[element].price + " x </td><td class=\"notImplemented\"><img src=\"assets/img/pencil.png\" alt=\"edit\" height=\"16px\"></td></tr>";
+		var tablerow = "<tr><td>" + categories[category].expenses[element].name + "</td><td class=\"listPrice\">" + categories[category].expenses[element].price + " €</td></tr>";
 
 		var tablerows= tablerows + tablerow;
 	}
 		var sum =  Math.round(sum * Math.pow(10, 2)) / Math.pow(10, 2);
-		var summedup= "<tr id=\"sum\"><td><span><b>Total Expenses 06/14</b></span></td><td id=\"totalsum\" class=\"listPrice\"><b>"+sum+" €</b></td</tr>";
-		var table = "<div>" + begintable + tablerows +  "</tbody>" + endtable + addbutton +"</div>";
+		var summedup= "<tr id=\"sum\"><td><span><b>Total Expenses 06/14</b></span></td><td id=\"totalsum\" class=\"listPrice\"><b>"+sum+" €</b></td></tr>";
+		var table = "<div>" + begintable + tablerows +  summedup + "</tbody>" + endtable + addbutton +"</div>";
 		return table;
 };
 
